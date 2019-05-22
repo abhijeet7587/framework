acfc4cb6ad5d6970e05668dd4a1d8f02d9166172
Abhijeet <Abhijeet@Dell>
2019-05-22 20:21:33 +0530

    commited

---
+++ practice/src/mock/Demo.java
@@ -0,0 +1,20 @@
+package mock;
+
+public class Demo 
+{	public static void main(String[] args)
+	{	//int a=5;
+		for(int i=5;i>=1;i--)
+		{
+			for(int j=5;j<=i;j--)
+			{
+				//if((j==1) ||(j==5)||(i==1)||(i==5))
+				System.out.print(j);
+				//else System.out.print(" ");
+			}
+			//a++;
+			System.out.println();
+		}
+	
+	}
+
+}
